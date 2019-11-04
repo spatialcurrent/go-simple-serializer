9bb8f07ef8df47b5e3e33df8e9fafa5d6d3d5b0e
Patrick Dufour <pjdufour.dev@gmail.com>
2019-11-04 09:25:21 -0500

    IsEmptyValue

--- pkg/mapper/IsEmptyValue.go
+++ pkg/mapper/IsEmptyValue.go
@@ -19,6 +19,20 @@ import (
 //  - If the value is of kind chan, func, interface, or pointer, and is not valid or nil, then returns true.
 //  - In all other cases, returns false.
 // IsEmptyValue is adapted from the isEmptyValue function in the `encoding/json` package, but adds support for additional types.
-func IsEmpty(v interface{}) bool {
-	return IsEmptyValue(reflect.ValueOf(v))
+func IsEmptyValue(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Chan, reflect.Func, reflect.Interface, reflect.Ptr:
+		return (!v.IsValid()) || v.IsNil()
+	}
+	return false
 }
